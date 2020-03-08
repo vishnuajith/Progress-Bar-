511854f74a16039962e3e4f4e748fd2093e2943e
Vishnu Ajith <45780187+vishnuajith@users.noreply.github.com>
2020-03-08 13:26:19 +0530

    Flies added

---
+++ progress bar.py
@@ -0,0 +1,23 @@
+import sys
+import time
+
+
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush() 
+
+
+
+i=0
+while(i<100):
+    progress(i,100, status='Doing very long job')
+    i+=1
+    time.sleep(0.2)
+
+
